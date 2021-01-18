7e859fdd201fac3a0cab72b0ac5de1e8fbea5e85
Kise7 <52307809+Kise7@users.noreply.github.com>
2021-01-18 21:15:49 +0800

    Add files via upload

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment.Models;
+
+namespace Assignment.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Autherize(Assignment.Models.User userModel)
+        {
+            using (EmployeeDatabaseEntities db = new EmployeeDatabaseEntities())
+            {
+                var userDetails = db.Users.Where(x => x.username == userModel.username && x.password == userModel.password).FirstOrDefault();
+                if(userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Wrong username or password.";
+                    return View("Index", userModel);
+                }
+                else
+                {
+                    Session["userID"] = userDetails.userID;
+                    Session["userName"] = userDetails.username;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+             
+        }
+        public ActionResult LogOut()
+        {
+            int userId = (int)Session["userID"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
